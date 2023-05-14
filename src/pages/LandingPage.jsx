a5b2159106442e621a194f8ade731c5813660579
atharfaz <101614789+atharfaz@users.noreply.github.com>
2023-05-14 22:33:24 +0700

    first-commit

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div className='flex flex-col'>
+      <div>navbar</div>
+      <div>hero</div>
+    </div>
+  )
+}
+
+export default LandingPage
